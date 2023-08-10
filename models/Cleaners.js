b35a87f0e73e4a7499b8055cc5ae762a0ad3fdf7
DC-21 <cholahkuboko@gmail.com>
2023-08-10 15:16:32 +0200

    first commit

---
+++ models/Cleaners.js
@@ -0,0 +1,31 @@
+const sequelize = require('../utils/db');
+const Sequelize = require('../utils/db');
+const { DataTypes, INTEGER, DECIMAL } = require('sequelize');
+
+const Cleaners = sequelize.define("Cleaners",{
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+    },
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    phone:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+    },
+    salary:{
+        type:DataTypes.DECIMAL(10,2),
+        allowNull:false,
+    },
+},{
+    timestamps:false,
+});
+
+module.exports= Cleaners;
