886f982b1d04b2b24ac9c0656b21dd8b0789ab56
DC-21 <cholahkuboko@gmail.com>
2023-08-10 15:05:23 +0200

    first commit

---
+++ models/CompanyFinance.js
@@ -0,0 +1,28 @@
+const sequelize = require('../utils/db');
+const { DataTypes, DECIMAL } = require('sequelize')
+
+const CompanyFinance = sequelize.define("CompanyFinance",{
+    id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        autoIncrement:true,
+        unique:true,
+    },
+    year:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+    },
+    Month:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    balance:{
+        type:DECIMAL(10,2),
+        allowNull:false,
+    },
+},{
+    timeStamps:false,
+}
+);
+
+module.exports = CompanyFinance;
