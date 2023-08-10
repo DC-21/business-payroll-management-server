56c2d040ba0228d3fa331c9a0e7341aa0b3944e0
DC-21 <cholahkuboko@gmail.com>
2023-08-10 08:53:56 +0200

    first commit

---
+++ models/Data.js
@@ -0,0 +1,24 @@
+const sequelize = require("../utils/db");
+const {DataTypes} = require('sequelize');
+
+const Data = sequelize.define('Data',{
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+  sourceType: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  sourceContent: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  extractedText: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+}, {
+  timestamps: false,
+})
