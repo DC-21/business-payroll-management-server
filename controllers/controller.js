8bcc227419b81c84e3fb4978ecb0c87bb0b7832d
DC-21 <cholahkuboko@gmail.com>
2023-08-10 12:22:03 +0200

    first commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,33 +1,64 @@
-const openai = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 const { extractPDFData, extractWebData } = require("./dataUtils");
 const Data = require("../models/Data");
 const axios = require("axios");
 
+const apiKey = "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";
+
+const configuration = new Configuration({
+  apiKey: apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
 async function askQuestion(req, res) {
   try {
-    const userQuestion = req.body.question;
+    const userQuestion = req.body.question; // Extract the user's question
 
-    const response = await axios.post(
-      "https://api.openai.com/v1/models",
-      {
-        prompt: userQuestion,
-        max_tokens: 50,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer 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",
-        },
-      }
-    );
+    // Fetch relevant data from your database based on the user's question
+    const relevantDataFromDB = await fetchRelevantDataFromDB(userQuestion);
 
-    const generatedResponse = response.data.choices[0].text.trim();
-    await saveResponseToDB(userQuestion, generatedResponse);
+    // Use the relevant data as a prompt for OpenAI API
+    const openaiResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: relevantDataFromDB,
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    const generatedResponse = openaiResponse.data.choices[0].text.trim();
 
     res.json({ response: generatedResponse });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function fetchRelevantDataFromDB(userQuestion) {
+  try {
+    // Fetch all entries from the database
+    const allData = await Data.findAll();
+    console.log(allData);
+
+    // Iterate through each entry and check if the user's question is relevant
+    const relevantData = [];
+    for (const entry of allData) {
+      const extractedText = entry.extractedText;
+      if (extractedText.includes(userQuestion)) {
+        relevantData.push(extractedText);
+      }
+    }
+
+    // Combine relevant data into a single string
+    const combinedRelevantData = relevantData.join("\n");
+
+    return combinedRelevantData;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error fetching relevant data from the database");
   }
 }
 
