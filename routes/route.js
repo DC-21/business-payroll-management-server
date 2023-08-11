c19ed456a2862c89e320ed68ef7441ca7e4fa33f
DC-21 <cholahkuboko@gmail.com>
2023-08-11 09:12:05 +0200

    first commit

--- routes/route.js
+++ routes/route.js
@@ -3,20 +3,30 @@ const router = express.Router();
 const controller = require('../controllers/controller');
 
 router.post('/signup', controller.signUp);
+router.get('/signup',controller.getSignup);
+
 router.post('/login', controller.login);
 router.put('/update-password', controller.updatePassword);
 router.delete('/delete-user', controller.deleteUser);
 
 router.post('/management', controller.management);
+router.get('/management',controller.getManagement);
+
 router.post('/it', controller.it);
+router.get('/it',controller.getIt);
+
 router.post('/cleaners', controller.cleaners);
 router.get('/cleaners',controller.getCleaners);
+
 router.post('/security', controller.security);
 router.get('/security',controller.getSecurity);
+
 router.post('/finance', controller.finance);
 router.get('/finance',controller.getFinance);
+
 router.post('/department',controller.departments);
 router.get('/department',controller.getDepartments);
+
 router.post('/company-finances', controller.companyFinance);
 router.get('/company-finances',controller.getCompanyFinance);
 
